4bcc27eba7e7e3bfaf22d83d7cc73581427fabf8
Ricardo Theodoro <rtheodoro@usp.br>
2023-09-09 12:23:15 -0300

    webscraping funcionando

--- README.md
+++ README.md
@@ -1,29 +1,41 @@
 # Webscrapping dos Orgãos Estatutários das Cooperativas de Crédito - BACEN
 
-Objetivo deste script foi aprender `Julia` fazendo download das informações das Cooperativas de Crédito do Bacen.
+Objetivo deste script é fazer o download das informações, mas utilizando a linguagem `Julia`.
 
 Este script é a tradução de um script em `R`, que pode ser encontrado em [aqui](https://github.com/rtheodoro/orgaos-estatutarios-coop-cred-bacen)
 
-***PROBLEMAS***: ainda não consegui extrair dados quando uma das cooperativas não possui a informação, como por exemplo: **classe, filiacao, telefone e ouvidoria**. Se uma cooperativa não possui classe, o script não consegue extrair as informações das demais cooperativas.
-
+## Como funciona
 
 As informações serão coletadas do endereço: [https://www.bcb.gov.br/estabilidadefinanceira/encontreinstituicao](https://www.bcb.gov.br/estabilidadefinanceira/encontreinstituicao)
 
-Ele irá criar uma pasta dentro de `dados` com o nome de `anomes` e irá gravar os arquivos nela.  
+Será criado uma pasta dentro de `dados` com o nome de `anomes` e irá gravar os arquivos nela.  
 
-Ele irá baixar as infomações: 
+Então serão baixadas as infomações: 
 
    info_gerais_coop:
    
       nome
+
+      classe
       
       naturezaJuridica
       
       situacao
       
-      segmentoPrudencial
+      regimeEspecial
       
       endereco
-
       
+      telefone
+      
+      segmentoPrudencial
+      
+      atoPresi
+      
+      nomeLiquidante
+      
+      filiacao
+      
+      ouvidoria
 
+   
--- webscraping_bacen_orgaosestatutarios.jl
+++ webscraping_bacen_orgaosestatutarios.jl
@@ -5,7 +5,7 @@ using CSV
 using DataFrames
 using JSON
 import Base
-
+using DataStructures 
 
 # Data da coleta
 datacoleta = Dates.format(now(), "yyyymm")
@@ -40,57 +40,41 @@ rename!(cnpj_coopcred, Symbol("CNPJ") => :cnpj)
 # Filtrar as cooperativas de crédito sem CNPJ
 cnpj_coopcred = filter(row -> !ismissing(row.cnpj), cnpj_coopcred)
 
-# Visualizar as primeiras linhas do DataFrame
-first(cnpj_coopcred, 5)
 
+# Baixando informações estatutarias -------------------------
 
-# Baixando informações estatutarias
+# Definindo vetor de cnpj que será buscado
 cnpj = cnpj_coopcred[!, :cnpj]
 
 # Carrega df Vazios
+cs = ["cnpj", "naturezaJuridica", "situacao", "regimeEspecial", "logradouro", "complemento", "bairro", "municipio", "siglaEstado", "cep", "email", "segmentoPrudencial", "filiacao", "tel_ouvidoria", "nome_ouvidor"]
 info_gerais_coop = DataFrame()
 
 for i in 1:length(cnpj)
-
+    
     println("CNPJ ", cnpj[i], " --- ", i, " de ", length(cnpj))
-
-    # Fetch JSON data
+    
     url = "https://www3.bcb.gov.br/informes/rest/pessoasJuridicas?cnpj=$(cnpj[i])"
     response = HTTP.get(url)
     dcoop = JSON.parse(String(response.body))
 
     println("CNPJ ", cnpj[i], " Info. Gerais")
 
-    # Extracting information
-    info_gerais_coop_i = DataFrame(
-    cnpj_coop = cnpj[i],
-    nome_coop = dcoop["nome"],
-    naturezaJuridica = String(get(dcoop, "naturezaJuridica", missing)),
-    #classe = get(get(get(dcoop, "classeCooperativa", nothing), "nome", nothing), 1),
-    situacao = String(get(dcoop, "situacao", missing)),
-    regimeEspecial = dcoop["regimeEspecial"],
-    logradouro = String(get(get(dcoop, "endereco", missing), "logradouro", missing)),
-    complemento = String(get(get(dcoop, "endereco", missing), "complemento", missing)),
-    bairro = String(get(get(dcoop, "endereco", missing), "bairro", missing)),
-    municipio = String(get(get(get(dcoop, "endereco", missing), "municipio", missing), "nome", missing)),
-    uf = String(get(get(get(dcoop, "endereco", missing), "municipio", missing), "siglaEstado", missing)),
-    cep = String(get(get(dcoop, "endereco", missing), "cep", missing)),
-    endereco_eletronico = String(get(get(dcoop, "endereco", missing), "email", missing)),
-    email = String(get(get(dcoop, "endereco", missing), "email", missing)),
-    segmento_prudencial = get(dcoop, "segmentoPrudencial", missing),
-    #filiacao = get(dcoop, "filiacoesCooperativaCentral", missing)
-)
-
-    # Check if info_gerais_coop_i exists and merge
-
-    if isempty(info_gerais_coop)
-        info_gerais_coop = copy(info_gerais_coop_i)
-    else
-        info_gerais_coop = vcat(info_gerais_coop, info_gerais_coop_i)
-        info_gerais_coop_i = nothing
-    end
+    dcoops = merge(dcoop, dcoop["endereco"], dcoop["endereco"]["municipio"])
+    new = DataFrame(filter(((k,v),) -> k in cs, dcoops))
+    new[!, :nome_coop] = [dcoop["nome"]]
+    new[!, :municipio] = [dcoop["endereco"]["municipio"]["nome"]]
+    new[!, :classe] = [!isnothing(dcoop["classeCooperativa"]) ? dcoop["classeCooperativa"]["nome"] : nothing]
+    new[!, :filiacao] = [!isnothing(dcoop["filiacao"]) ? dcoop["filiacao"] : nothing]
+    new[!, :tel_ouvidoria] = [!isnothing(dcoop["ouvidoria"]) ? dcoop["ouvidoria"]["telefone"] : nothing]
+    new[!, :nome_ouvidoria] = [!isnothing(dcoop["ouvidoria"]) ? dcoop["ouvidoria"]["nomeOuvidor"] : nothing]
+    info_gerais_coop = [info_gerais_coop;new]
         
 
 end
+
+# Organizar as colunas na ordem desejada
+info_gerais_coop = info_gerais_coop[:, Cols(:cnpj, :nome_coop, :classe, :)]
+
 
 CSV.write("dados/$(datacoleta)/info_gerais.csv", info_gerais_coop; transform = (col, val) -> isnothing(val) ? missing : val)
